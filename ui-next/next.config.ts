5c8e59cae72c7bd00a92cdb2f35681a16114dc76
Ravi Raj <raviraj14y@gmail.com>
2026-04-07 22:20:56 +0530

    fixed urls

--- ui-next/next.config.ts
+++ ui-next/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig: NextConfig = {
-  basePath: '/job-tracker',
   output: "standalone",
   devIndicators: false,
 
